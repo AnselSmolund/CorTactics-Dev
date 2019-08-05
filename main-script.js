95e189da2d75baba943c97eb8b14faf8179cf81e
Ansel Smolund <smolu006@umn.edu>
2019-08-04 20:10:19 -0500

    removing video from landing page

--- main-script.js
+++ main-script.js
@@ -2,51 +2,51 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
-var timeout = null;
-var movable = true;
-$(document).on('mousemove', function() {
-    if (timeout !== null) {
-        clearTimeout(timeout);
-    }
-    movable = true;
-    timeout = setTimeout(function() {
-        $('.navbar').css({'transform' : 'translateY(0)'});
-        $('#title-header').css({'opacity' : '1'});
-        $('#bgVideo').css({'transition': '.5s'});
-        $('#bgVideo').css({'transform': 'scale(1.2)'});
-        movable = false;
+// var timeout = null;
+// var movable = true;
+// $(document).on('mousemove', function() {
+//     if (timeout !== null) {
+//         clearTimeout(timeout);
+//     }
+//     movable = true;
+//     timeout = setTimeout(function() {
+//         $('.navbar').css({'transform' : 'translateY(0)'});
+//         $('#title-header').css({'opacity' : '1'});
+//         $('#bgVideo').css({'transition': '.5s'});
+//         $('#bgVideo').css({'transform': 'scale(1.2)'});
+//         movable = false;
 
-    }, 500);
-});
-$(function ()
-{
-    $("#main-cover").mouseenter(function () {
-        console.log("entering");
-        $('#bgVideo').css({'transition': '1s'});
-        $('#bgVideo').css({'transform': 'scale(1.4)'});
+//     }, 500);
+// });
+// $(function ()
+// {
+//     $("#main-cover").mouseenter(function () {
+//         console.log("entering");
+//         $('#bgVideo').css({'transition': '1s'});
+//         $('#bgVideo').css({'transform': 'scale(1.4)'});
     
-    }).mouseleave(function (){
-        $('#bgVideo').css({'transition': '.5s'});
-        $('#bgVideo').css({'transform': 'scale(1.2)'});
-    }).mousemove(function(e){
-        $('#bgVideo').css({'transition': '.5s'});
-        $('#bgVideo').css({'transform': 'scale(1.4)'});
+//     // }).mouseleave(function (){
+//     //     $('#bgVideo').css({'transition': '.5s'});
+//     //     $('#bgVideo').css({'transform': 'scale(1.2)'});
+//     // }).mousemove(function(e){
+//     //     $('#bgVideo').css({'transition': '.5s'});
+//     //     $('#bgVideo').css({'transform': 'scale(1.4)'});
         
-        $('.navbar').css({'transform' : 'translateY(-100%)'});
-        $('#title-header').css({'opacity' : '.7'});
-        if(movable){
-            $('#bgVideo').css({'transition': '0s'});
-            $('#bgVideo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()/1.03) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()/1.03) * 100 +'%'}); 
-        }
-        $('.navbar').css({'transition' : '.5s'});
-        var top = (window.pageYOffset);
-        console.log(top);
-        if(e.pageY < top + 100){
-            $('.navbar').css({'transition' : '.1s ease-in'});
-            $('.navbar').css({'transform' : 'translateY(0%)'});
-        }
-    });
-});
+//     //     $('.navbar').css({'transform' : 'translateY(-100%)'});
+//     //     $('#title-header').css({'opacity' : '.7'});
+//     //     if(movable){
+//     //         $('#bgVideo').css({'transition': '0s'});
+//     //         $('#bgVideo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()/1.03) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()/1.03) * 100 +'%'}); 
+//     //     }
+//     //     $('.navbar').css({'transition' : '.5s'});
+//     //     var top = (window.pageYOffset);
+//     //     console.log(top);
+//     //     if(e.pageY < top + 100){
+//     //         $('.navbar').css({'transition' : '.1s ease-in'});
+//     //         $('.navbar').css({'transform' : 'translateY(0%)'});
+//     //     }
+//     // });
+// });
 
 $(document).scroll(function() {
     $('.navbar').css({'transform' : 'translateY(0)'});
